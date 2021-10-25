4a70aa28439ca6c65601f4ae43bccda9eb90ae95
Thomas Klebel <t.klebel@hotmail.com>
2021-10-25 15:30:45 +0200

    visualise publication distribution with log-log

--- analysis/01-calibration-exploration.R
+++ analysis/01-calibration-exploration.R
@@ -15,3 +15,34 @@ leiden_select %>%
   facet_wrap(vars(Period))
 
 ggsave(filename = "plots/pub_dist_leiden.png")
+
+
+# visualise lotkas or zipfs law
+# https://en.wikipedia.org/wiki/Zipf%27s_law
+# https://en.wikipedia.org/wiki/Lotka%27s_law
+leiden_select %>% 
+  filter(Period == "2015–2018") %>% 
+  mutate(uni_rank = rank(impact_P)) %>% 
+  ggplot(aes(uni_rank, impact_P)) +
+  geom_point()
+
+
+leiden_select %>% 
+  filter(Period == "2015–2018") %>% 
+  mutate(uni_rank = rank(impact_P),
+         uni_rank = max(uni_rank) - uni_rank + 1) %>% 
+  ggplot(aes(uni_rank, impact_P)) +
+  geom_point() +
+  scale_y_log10() +
+  scale_x_log10()
+
+# this does not work
+leiden_select %>% 
+  filter(Period == "2015–2018") %>% 
+  mutate(uni_rank = rank(impact_P),
+         uni_rank = max(uni_rank) - uni_rank + 1) %>% 
+  ggplot(aes(uni_rank, impact_P)) +
+  geom_point() +
+  geom_point(aes(y = 1/uni_rank), colour = "red") +
+  scale_y_log10() +
+  scale_x_log10()
