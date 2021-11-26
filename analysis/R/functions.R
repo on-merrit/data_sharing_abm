7f0275586bec7429d18494571da511e642ecb7e6
Thomas Klebel <t.klebel@hotmail.com>
2021-11-26 17:28:29 +0100

    analyse new data

--- analysis/R/functions.R
+++ analysis/R/functions.R
@@ -5,8 +5,10 @@ read_experiments <- function(path) {
 }
 
 read_nested_experiment <- function(path, 
-                                   columns = c("who", "data_grant_share", 
-                                               "n_publications", "total_grants")) {
+                                   columns = c(
+                                     "who", "data_grant_share", 
+                                     "n_publications", "n_pubs_with_data_shared",
+                                     "total_grants")) {
   df <- read_csv(path, skip = 6) %>% 
     tibble(.name_repair = "universal") 
   
--- analysis/stuff.R
+++ analysis/stuff.R
@@ -1,24 +1,10 @@
 path <- "../outputs/grant-balance-on-publications-only-end"
 
-df <- read_csv(path, skip = 6) %>% 
-  tibble(.name_repair = "universal")
-
-df <- df %>% 
-  select(run = .run.number., step = .step., stuff)
-
-df_extracted <- df %>% 
-  mutate(stuff = str_remove_all(stuff, "(^\\[\\[)|(\\]\\]$)")) %>% 
-  separate(stuff, paste0("group", 1:100), sep = "\\] \\[") %>% 
-  pivot_longer(starts_with("group"), names_to = "group", values_to = "vals") %>% 
-  separate(vals, c("who", "data_grant_share", "n_publications", "total_grants"),
-           sep = "\\s")
-  
+df_extracted <- read_nested_experiment(path)
 
 
 df_extracted <- df_extracted %>% 
-  mutate(across(c("who", "data_grant_share", "n_publications", "total_grants"),
-                as.numeric))
-
+  select(run, step, who:total_grants)
 
 df_extracted %>% 
   ggplot(aes(n_publications, data_grant_share)) +
@@ -37,3 +23,47 @@ m2 <- lm(n_publications ~ total_grants + data_grant_share +
            I(data_grant_share^2),
          data = df_extracted)
 summary(m2)
+
+
+# also simply look at publication distributions
+df_extracted %>% 
+  ggplot(aes(n_publications, group = run)) +
+  geom_density()
+
+df_extracted %>% 
+  group_by(run) %>% 
+  summarise(ginis = ineq::Gini(n_publications)) %>% 
+  ggplot(aes(ginis)) +
+  geom_density()
+# distribution of ginis
+
+
+# now look at the two settings (funding based on history or not)
+df_rewards <- read_nested_experiment(
+  "../outputs/data_sharing benefit-of-awarding-data-sharing-table.csv"
+)
+
+df_rewards <- df_rewards %>% 
+  select(run, pubs.vs.data, step, who:total_grants)
+df_rewards
+
+df_rewards %>% 
+  ggplot(aes(n_publications, group = run, fill = pubs.vs.data)) +
+  geom_density(alpha = .2)
+
+df_rewards %>% 
+  ggplot(aes(n_publications, group = pubs.vs.data, colour = pubs.vs.data)) +
+  geom_density(alpha = .5)
+
+df_rewards %>% 
+  ggplot(aes(data_grant_share, group = run, colour = pubs.vs.data)) +
+  geom_density(alpha = .5)
+
+df_rewards %>% 
+  ggplot(aes(data_grant_share, colour = factor(pubs.vs.data), 
+             group = pubs.vs.data)) +
+  geom_density(alpha = .2, adjust = 1.5)
+# it seems the distribution is fatter for more weight on data sharing
+# this effect might be stronger if we had more funders. maybe we would then have
+# a separation? hard to say, because simulation would likely be very slow
+# would need to profile
